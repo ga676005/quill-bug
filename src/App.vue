071bdc02337cbf674176765044c71cc2b9028746
gohomewho <ga676005@hotmail.com.tw>
2022-06-17 16:06:51 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import { ref } from 'vue';
+
+const showEditor = ref(true)
+const text = ref('')
+</script>
+
+<template>
+  <button @click="showEditor = !showEditor">toggle</button>
+
+  <QuillEditor v-model:content="text" toolbar="#my-toolbar" v-if="showEditor" theme="snow" content-type="html">
+    <template #toolbar>
+      <div id="my-toolbar">
+        <span class="ql-formats">
+          <select class="ql-font"></select>
+          <select class="ql-size"></select>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-bold"></button>
+          <button class="ql-italic"></button>
+          <button class="ql-underline"></button>
+          <button class="ql-strike"></button>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-list" value="ordered"></button>
+          <button class="ql-list" value="bullet"></button>
+          <button class="ql-indent" value="-1"></button>
+          <button class="ql-indent" value="+1"></button>
+        </span>
+        <span class="ql-formats">
+          <select class="ql-align"></select>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-link"></button>
+          <button class="ql-image"></button>
+          <button class="ql-video"></button>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-clean"></button>
+        </span>
+      </div>
+    </template>
+  </QuillEditor>
+</template>
+
+<style>
+</style>
